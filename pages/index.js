c86fc32f6e5886364d74360a03e7f04682b5d0b0
Ian Camargo <ian.camargo.andrade@gmail.com>
2022-02-09 19:24:33 -0300

    att3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React,{ useState } from 'react';
-
+import styles from './index.module.css'
 
 
 export default function App() {
@@ -7,24 +7,26 @@ export default function App() {
   const[resultado,setResultado]=useState(0)
   const[acumulador,setAcumulador]=useState(0)
   const[operado,setOperado]=useState(false)
+  const[cont,setCont]=useState(0)
 
   //COMPONENTES
   const Tela=(valor,resultado)=>{
     return(
-      <div style={visor}>
-        <span style={value}>{valor}</span>
-        <span style={res}>{resultado}</span>
+      <div className={styles.visor}>
+        <span className={styles.value}>{valor}</span>
+        <span className={styles.res}>{resultado}</span>
       </div>
     )
   }
   const btn=(label,onCLick)=>{
     return(
-      <button style={botao} onClick={onCLick}>{label}</button>
+      <button className={styles.botao} onClick={onCLick}>{label}</button>
     )
   }
 
   //FUNÇÕES
   const addDigitoTela=(d)=>{
+    setCont(0)
     if((d=='+'||d=='-'||d=='*'||d=='/')&& operado){
       setOperado(false)
       setValorTela(resultado+d)
@@ -37,15 +39,38 @@ export default function App() {
     }
     const valorDigitadoTela=valorTela+d
     setValorTela(valorDigitadoTela)
+    
+  }
+  const oper=(o)=>{
+    if((o=='+'||o=='-'||o=='*'||o=='/') && cont<1){
+      addDigitoTela(o)
+      setCont(1)
+      return
+    }
+    else{
+      setValorTela('ERRO')
+      return
+    }
   }
   const limparMemoria=()=>{
+    setCont(0)
     setOperado(false)
     setValorTela('')
     setResultado(0)
     setAcumulador(0)
     return
   }
+  const limparvTela=()=>{
+    setCont(0)
+    setOperado(true)
+    setValorTela('')
+    setResultado(resultado)
+    setAcumulador(0)
+    return
+  }
+  
   const Operacao=(oper)=>{
+    setCont(0)
     if(oper=='bs'){
       let vtela=valorTela
       vtela=vtela.substring(0,(vtela.length-1))
@@ -60,65 +85,25 @@ export default function App() {
       setOperado(true)
     }catch{
       setResultado('ERRO')
+      setValorTela('')
     }    
-  }
-  
-  //STYLE
-  const container={
-    display: 'flex',
-    justifyContent:'flex-start',
-    alignItems:'center',
-    flexDirection:'column',
-    width: 300,
-    border: '1px solid #000'
-  }
-  const but={
-    flexDirection:'row',
-    flexWrap:'wrap',
-
-  }
-  const visor={
-    display: 'flex',
-    paddingleft:20,
-    paddingright:20,
-    justifyContent:'center',
-    alignItems:'flex-start',
-    backgroundColor:'#444',
-    flexDirection:'column',
-    width:300,
-  }
-  const value={
-    fontSize: 25,
-    color:'#fff',
-    height:20 ,
-  }
-  const res={
-    fontSize: 35,
-    color:'#fff',
-    height:35,
-    
-  }
-  const botao={
-    fontSize:30,
-    height:75,
-    width:75,
-    padding:20,
-    backgroundColor:'#000',
-    color:'#fff',
-    textAlign:'center',
   }
   
   //CONTAINER
 
   return (
     <>
-      <div style={container}>
+      <div className={styles.container}>
         <h3>Calculadora</h3>
         {Tela(valorTela,resultado)}
-        <div style={but}>
+        <div className={styles.but}>
+          {btn('%',limparvTela)}
+          {btn('C',limparvTela)}
           {btn('AC',limparMemoria)}
+          {btn('<-',()=>Operacao('bs'))}
           {btn('(',()=>addDigitoTela('('))}
           {btn(')',()=>addDigitoTela(')'))}
+          {btn('²√x',()=>addDigitoTela(')'))}
           {btn('/',()=>addDigitoTela('/'))}
           {btn('7',()=>addDigitoTela('7'))}
           {btn('8',()=>addDigitoTela('8'))}
@@ -132,9 +117,9 @@ export default function App() {
           {btn('2',()=>addDigitoTela('2'))}
           {btn('3',()=>addDigitoTela('3'))}
           {btn('+',()=>addDigitoTela('+'))}
+          {btn('+/-',()=>addDigitoTela(')'))}
           {btn('.',()=>addDigitoTela('.'))}
           {btn('0',()=>addDigitoTela('0'))}
-          {btn('<-',()=>Operacao('bs'))}
           {btn('=',()=>Operacao('='))}
         </div>
       </div>
